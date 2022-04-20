8b12428a46e61a5109f072df727db6808770a9de
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-20 16:45:17 -0300

    Resolução do requisito 3

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const paletaCores = document.getElementById('color-palette');
 const filhosPaletaCores = paletaCores.children;
 const listaCores = document.getElementById('lista-cores');
 const itensListaCores = listaCores.children;
-let cores = ['red', 'blue', 'black', 'gray', 'silver', 'green', 'purple', 'yellow', 'brown', 'orange', 'pink'];
+let cores = ['red', 'blue', 'gray', 'silver', 'green', 'purple', 'yellow', 'brown', 'orange', 'pink'];
 function coresSeletor() {
-    for (index = 0; index < filhosPaletaCores.length; index += 1) {
-        let sorteio = Math.floor(Math.random()* (11 - [index]));
+    filhosPaletaCores[0].style.backgroundColor = 'black';
+    let itemLista = document.createElement('li');
+    itemLista.innerHTML = 'black';
+    itemLista.style.color = 'black';
+    itensListaCores[0].appendChild(itemLista);
+    for (index = 1; index < filhosPaletaCores.length; index += 1) {
+        let sorteio = Math.floor(Math.random()* (10 - [index]));
         filhosPaletaCores[index].style.backgroundColor = cores[sorteio];
         let itemLista = document.createElement('li');
         itemLista.innerHTML = cores[sorteio];
